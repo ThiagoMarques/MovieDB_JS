a1385fe09c5336d25963905786c43aaf216fd367
ThiagoMarques <thiagossmarques@gmail.com>
2023-03-16 21:49:52 -0300

    feat: insere lista de filmes principais

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,39 @@
 const API_KEY = '898915155b5e55642efcc67317e973a0';
 const version = 3;
-const path = 'movie/popular';
+let pathTopMovie = 'movie/popular';
 const language = 'pt-BR';
-const url = `https://api.themoviedb.org/${version}/${path}?api_key=898915155b5e55642efcc67317e973a0&language=${language}`
+const urlMain = `https://api.themoviedb.org/${version}/${pathTopMovie}?api_key=898915155b5e55642efcc67317e973a0&language=${language}`
+const urlImage = `https://image.tmdb.org/t/p/original`
 
+function convertTopMoviesToHTML(movie) {
+    return `
+    <div class="section__movie">
+        <div class="section__movie-image">
+            <img
+                src="${urlImage}/${movie.poster_path}"
+                alt="Poster do filme ${movie.title}"
+            />
+        </div>
+        <div class="section__movie-details">
+            <li class="details__title">${movie.title}</li>
+            <li class="details__original-language">Linguagem: ${movie.original_language}
+            </li>
+            <li class="details__vote-average">Avaliação: ${movie.vote_average}</li>
+        </div>
+    </div>`
+}
 
-fetch(url)
+const topMoviesElem = document.getElementById('teste');
+
+fetch(urlMain)
     .then((response) => response.json() 
     .then((jsonBody) => jsonBody.results)
     .then((topMoviesList) => {
 
-        console.log(topMoviesList)
+        for (let index = 0; index < topMoviesList.length; index++) {
+            const topMovie = topMoviesList[index];
+            console.log('topMovie', topMovie)
+            topMoviesElem.innerHTML += convertTopMoviesToHTML(topMovie)
+        }
     })
     .catch((error) => console.error(error)))
